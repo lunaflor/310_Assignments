be1cc3916216d46080137079fc0029166607353f
lunaflor <160687635+lunaflor@users.noreply.github.com>
2024-05-16 16:10:52 -0700

    Update Final.c

--- Assignments/Final.c
+++ Assignments/Final.c
@@ -1,27 +1,29 @@
 #include <xc.h>
 #include <stdio.h>
 #include <string.h>
-#include "PWM.h" // must have this
+#include "PWM.h" // Must have this for PWM functionality
+
+//In this code we utlize PWM for the buzzer, interrupt when going under a threshold for the analog sensor, and ADC to change LCD dsplay based on different voltages.
 
 #define _XTAL_FREQ 4000000 // Fosc frequency for _delay() library
 #define FCY _XTAL_FREQ / 4
 
-#define RS LATB0       /* PORTB 0 pin is used for Register Select */
-#define EN LATB1       /* PORTB 1 pin is used for Enable */
-#define ldata LATD     /* PORTD is used for transmitting data to LCD */
-#define BUZZER LATAbits.LATA5   /* PORTA 5 pin is used for Buzzer */
+#define RS LATB0       // PORTB 0 pin for Register Select
+#define EN LATB1       // PORTB 1 pin for Enable
+#define ldata LATD     // PORTD for LCD data
+#define BUZZER LATAbits.LATA5   // PORTA 5 pin for Buzzer
 #define BUZZER_TRIS TRISAbits.TRISA5
 #define LCD_Port TRISD
 #define LCD_Control TRISB
-#define Vref 5.0 // voltage reference
+#define Vref 5.0 // Reference voltage
 #define PWM2_INITIALIZE_DUTY_VALUE 100
 
 uint16_t checkdutyCycle;
 char preScale;
 _Bool pwmStatus;
 
-int digital;    // Holds the digital value
-float voltage;  // Holds the analog value (in volts)
+int digital;    // Holds the digital value from ADC
+float voltage;  // Holds the analog voltage value
 char data[10];  // Buffer to store the display string
 
 // Function prototypes
@@ -41,21 +43,21 @@ void __interrupt(irq(IRQ_INT2), base(0x4008)) INT2_ISR(void);
 
 void main(void) {
     // Initialize peripherals
-    OSCSTATbits.HFOR = 1; // enable HFINTOSC Oscillator (see clock schematic)
-    OSCFRQ = 0x02; // 00=1 MHZ, 02=4MHZ internal - see page 106 of data sheet
+    OSCSTATbits.HFOR = 1; // Enable HFINTOSC Oscillator
+    OSCFRQ = 0x02; // Set internal frequency to 4MHz (see page 106 of datasheet)
     
     ANSELA = 0b00000000;    
-    TRISA = 0b00000000; // sets PORTA as all outputs 
-    PORTA = 0b00000000; // turns off PORTA outputs so that the LED is initially off
+    TRISA = 0b00000000; // Set PORTA as all outputs 
+    PORTA = 0b00000000; // Turn off PORTA outputs initially
     TMR2_Initialize();
     TMR2_StartTimer(); 
     PWM_Output_D8_Enable();
     PWM2_Initialize();
-    PWM2_LoadDutyValue(PWM2_INITIALIZE_DUTY_VALUE); // initialize CCPR2H/L
+    PWM2_LoadDutyValue(PWM2_INITIALIZE_DUTY_VALUE); // Initialize PWM duty cycle
 
-    // Duty Cycle in percentage 
+    // Calculate Duty Cycle in percentage 
     checkdutyCycle = (uint16_t)((100UL * PWM2_INITIALIZE_DUTY_VALUE) / (4 * (T2PR + 1)));
-    // binary value of Register T2CON.PRESCALE
+    // Get prescale value from T2CON register
     preScale = ((T2CON >> 4) & (0x0F)); 
 
     LCD_Init();
@@ -63,7 +65,7 @@ void main(void) {
     Buzzer_Init();
     INTERRUPT_Initialize();
 
-    // Ensure the buzzer is off and the display shows normal status
+    // Make sure the buzzer is off and display normal status
     Buzzer_Off();
     LCD_Command(0x01); // Clear display screen
     LCD_String_xy(0, 0, "System Normal");
@@ -71,25 +73,25 @@ void main(void) {
     while (1) {
         // Perform ADC reading and update the display
         ADC_Read();
-        MSdelay(500); // Delay for 500 ms between updates to reduce flickering
+        MSdelay(500); // Delay to reduce display flickering
     }
 }
 
 void LCD_Init(void) {
-    MSdelay(20);           // 20ms, 16x2 LCD Power-on delay
-    LCD_Port = 0x00;       // Set PORTD as output PORT for LCD data (D0-D7) pins
-    LCD_Control = 0x00;    // Set PORTB as output PORT for LCD Control (RS, EN) pins
-    LCD_Command(0x38);     // Use 2 lines and initialize 5x7 matrix of LCD
+    MSdelay(20);           // 20ms delay for LCD power-on
+    LCD_Port = 0x00;       // Set PORTD as output for LCD data
+    LCD_Control = 0x00;    // Set PORTB as output for LCD control
+    LCD_Command(0x38);     // Initialize LCD in 2-line mode with 5x7 matrix
     MSdelay(5);
     LCD_Command(0x0c);     // Display ON, cursor OFF
     MSdelay(5);
-    LCD_Command(0x06);     // Increment cursor (shift cursor to right)
+    LCD_Command(0x06);     // Increment cursor to the right
     MSdelay(5);
 }
 
 void LCD_Command(char cmd) {
-    ldata = cmd;           // Send data to PORT as a command for LCD
-    RS = 0;                // Command Register is selected
+    ldata = cmd;           // Send command to PORT
+    RS = 0;                // Select Command Register
     EN = 1;                // High-to-low pulse on Enable pin to latch data
     __delay_us(1);         // Short delay for data latch
     EN = 0;
@@ -98,7 +100,7 @@ void LCD_Command(char cmd) {
 
 void LCD_Char(char dat) {
     ldata = dat;           // Send data to LCD
-    RS = 1;                // Data Register is selected
+    RS = 1;                // Select Data Register
     EN = 1;                // High-to-low pulse on Enable pin to latch data
     __delay_us(1);         // Short delay for data latch
     EN = 0;
@@ -107,7 +109,7 @@ void LCD_Char(char dat) {
 
 void LCD_String(const char *msg) {
     while (*msg != 0) {
-        LCD_Char(*msg);    // Send each character of the string to the LCD
+        LCD_Char(*msg);    // Send each character to the LCD
         msg++;
     }
 }
@@ -115,9 +117,9 @@ void LCD_String(const char *msg) {
 void LCD_String_xy(char row, char pos, const char *msg) {
     char location = 0;
     if (row == 0) {
-        location = 0x80 | (pos & 0x0F); // Print on the 1st row at the desired position
+        location = 0x80 | (pos & 0x0F); // Set location on 1st row
     } else {
-        location = 0xC0 | (pos & 0x0F); // Print on the 2nd row at the desired position
+        location = 0xC0 | (pos & 0x0F); // Set location on 2nd row
     }
     LCD_Command(location);
     LCD_String(msg);
@@ -126,7 +128,7 @@ void LCD_String_xy(char row, char pos, const char *msg) {
 void MSdelay(unsigned int val) {
     unsigned int i, j;
     for (i = 0; i < val; i++) {
-        for (j = 0; j < 165; j++); // Provide a delay of 1 ms for 4 MHz Frequency
+        for (j = 0; j < 165; j++); // 1 ms delay for 4 MHz frequency
     }
 }
 
@@ -135,8 +137,13 @@ void Buzzer_Init(void) {
 }
 
 void Buzzer_On(void) {
-    //BUZZER = 1; // Turn on the buzzer
-    PWM2_LoadDutyValue(512); // Set PWM duty cycle to 50% to turn on the buzzer
+    pwmStatus = PWM2_OutputStatusGet();
+    PORTAbits.RA1 = pwmStatus;
+    // Stop the timer and handle the interrupt
+    if (PIR4bits.TMR2IF == 1) {
+        PIR4bits.TMR2IF = 0;
+        BUZZER = 1; // Turn on the buzzer
+    }
 }
 
 void Buzzer_Off(void) {
@@ -158,7 +165,7 @@ void INTERRUPT_Initialize(void) {
 
 void __interrupt(irq(IRQ_INT2), base(0x4008)) INT2_ISR(void) {
     if (PIR7bits.INT2IF) {  // Check if INT2 interrupt flag is set
-        // Handle the interruption
+        // Handle the interrupt
         Buzzer_On();
         
         sprintf(data, "V: %.2f", voltage);
@@ -179,11 +186,11 @@ void ADC_Read(void) {
     // Calculate the voltage value
     voltage = digital * (Vref / 4096.0);
 
-    // Update the LCD only if the voltage value changes significantly
+    // Update the LCD if the voltage value changes significantly
     static float last_voltage = -1;
     if (voltage < 0.5) {
-        // Enable the interrupt manually
-        PIR7bits.INT2IF = 1; // Set the interrupt flag to trigger the ISR
+        // Manually trigger the interrupt
+        PIR7bits.INT2IF = 1; // Set the interrupt flag
     } else if ((voltage - last_voltage) > 0.5) {
         sprintf(data, "V: %.2f", voltage);
         LCD_Command(0x80); // Move cursor to first row
@@ -196,19 +203,19 @@ void ADC_Read(void) {
 
 void ADC_Init(void) {
     // Setup ADC
-    ADCON0bits.FM = 1;  // right justify
+    ADCON0bits.FM = 1;  // Right justify
     ADCON0bits.CS = 1; // ADCRC Clock
 
     TRISAbits.TRISA0 = 1; // Set RA0 to input
     ANSELAbits.ANSELA0 = 1; // Set RA0 to analog
 
     ADPCH = 0x00; // Set RA0 as Analog channel in ADC ADPCH
-    ADCLK = 0x00; // Set ADC CLOCK Selection register to zero
+    ADCLK = 0x00; // Set ADC Clock Selection register to zero
 
     ADRESH = 0x00; // Clear ADC Result registers
     ADRESL = 0x00; 
 
-    ADPREL = 0x00; // Set precharge select to 0 in register ADPERL & ADPERH
+    ADPREL = 0x00; // Set precharge select to 0 in ADPERL & ADPERH
     ADPREH = 0x00; 
 
     ADACQL = 0x00;  // Set acquisition low and high byte to zero 
@@ -216,3 +223,4 @@ void ADC_Init(void) {
     
     ADCON0bits.ON = 1; // Turn ADC On 
 }
+
